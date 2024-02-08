fc339ee816ebad64143c92d4f80a343a4b808279
NahuelHawk <nmuchetti@purpletreestudio.com>
2024-02-08 19:24:43 -0300

    encapsulado interaction que antes estaba en character custom

---
+++ Assets/Interactions/Editor/InteractionCustomEditor.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UIElements;
+using System;
+public class InteractionCustomEditor : Editor
+{
+    Dictionary<Interaction, SubtypeSelector> subTypeSelectors;
+
+    CustomListView<Interaction> listCustom;
+    List<Interaction> interactions;
+
+    public void ShowGUI(VisualElement root, UnityEngine.Object myTarget) {
+        subTypeSelectors = new Dictionary<Interaction, SubtypeSelector>();
+
+        root.Q("InteractionSelect").visible = false;
+        root.Q("InteractionSelect").StretchToParentSize();
+
+        listCustom = new();
+
+        if (myTarget is RoomInteractuable)
+        { 
+            listCustom.ItemsSource = ((RoomInteractuable)myTarget).interactions;
+            interactions = ((RoomInteractuable)myTarget).interactions;
+        }
+
+        Func<int, VisualElement> itemContent = (i) =>
+        {
+            int index = i;
+
+            InteractionSelect select3 = (InteractionSelect)CreateInstance(typeof(InteractionSelect));
+            VisualElement visualElem = new VisualElement();
+            visualElem.Add(select3.ShowAndConfigure(interactions[index]));
+            select3.OnChangeTypeEvent += (inter) => { UpdateSelector(inter, index, visualElem); };
+            UpdateSelector(interactions[index], index, visualElem);
+            return visualElem;
+        };
+
+        listCustom.ItemContent = itemContent;
+
+        listCustom.ItemHeight = itemHeight;
+
+        //listCustom.OnChangeItem += OnChange;
+
+        listCustom.OnAdd = () => OnAdded(myTarget);
+
+        listCustom.OnReorderItem += (evt, element, index) => { SaveTargetChanges(myTarget); };
+
+        listCustom.OnRemoveItem += (element, index) => { SaveTargetChanges(myTarget); };
+
+        root.RegisterCallback<ChangeEvent<string>>((evt) => { SaveTargetChanges(myTarget); });
+
+        listCustom.reOrderMode = CustomListView<Interaction>.ReOrderModes.animatedDynamic;
+
+
+
+
+        listCustom.Init(root.Q<VisualElement>("CustomListView"));
+    }
+
+    private void UpdateSelector(Interaction interaction, int index, VisualElement element)
+    {
+        if (interaction != interactions[index]) return;
+
+
+        if (!string.IsNullOrEmpty(interaction.type))
+        {
+            if (interaction.type == "Inventory" && !subTypeSelectors.ContainsKey(interaction))
+            {
+                subTypeSelectors.Add(interaction, (SelectInventory)CreateInstance(typeof(SelectInventory)));
+            }
+            else if (interaction.type != "Inventory" && subTypeSelectors.ContainsKey(interaction))
+            {
+                ((SelectInventory)subTypeSelectors[interaction]).Remove(element);
+                subTypeSelectors.Remove(interaction);
+            }
+        }
+
+        if (subTypeSelectors.ContainsKey(interaction))
+        {
+            ((SelectInventory)subTypeSelectors[interaction]).Add(interaction, element);
+        }
+    }
+
+    private void SaveTargetChanges(UnityEngine.Object myTarget)
+    {
+        EditorUtility.SetDirty(myTarget);
+    }
+
+    private Interaction OnAdded(UnityEngine.Object myTarget)
+    {
+        SaveTargetChanges(myTarget);
+        return new Interaction();
+    }
+
+    private StyleLength itemHeight(int index)
+    {
+        return new StyleLength(StyleKeyword.Auto);
+    }
+}
--- Assets/Scripts/Scene/Editor/CharacterCustomEditor.cs
+++ Assets/Scripts/Scene/Editor/CharacterCustomEditor.cs
@@ -12,115 +12,42 @@ public class CharacterCustomEditor : Editor
     VisualElement root;
     RoomInteractuable myTarget;
     [SerializeField] VisualTreeAsset visualTree;
-    Dictionary<Interaction, SubtypeSelector> subTypeSelectors;
 
-    CustomListView<Interaction> listCustom;
+    //cambiar colores multiplicando por alfa o sumando varios
+    //falta un recuadro
+    //falta seleccion multiple
+    //falta que elimine lo seleccionado
+    //DONE
+    //probar uxml compuestos
+    //falta reordenamiento en realtime y animado
+    //falta que haga highlight cuando pasas el mouse
+    //falta poder seleccionar
 
     public override VisualElement CreateInspectorGUI()
     {
-        subTypeSelectors = new Dictionary<Interaction, SubtypeSelector>();
-
         myTarget = (RoomInteractuable)target;
 
         VisualElement root = visualTree.CloneTree();
 
-        root.Q("InteractionSelect").visible = false;
-        root.Q("InteractionSelect").StretchToParentSize();
+        InteractionCustomEditor interaction = (InteractionCustomEditor)CreateInstance(typeof(InteractionCustomEditor));
 
-        listCustom = new();
-        listCustom.ItemsSource = myTarget.interactions;
-
-        Func<int, VisualElement> itemContent = (i) =>
-        {
-            int index = i;
-
-            InteractionSelect select3 = (InteractionSelect)CreateInstance(typeof(InteractionSelect));
-            VisualElement visualElem = new VisualElement();
-            visualElem.Add(select3.ShowAndConfigure(myTarget.interactions[index]));
-            select3.OnChangeTypeEvent += (inter) => { UpdateSelector(inter, index, visualElem); };
-            UpdateSelector(myTarget.interactions[index], index, visualElem);
-            return visualElem;
-        };
-
-        listCustom.ItemContent = itemContent;
-
-        listCustom.ItemHeight = itemHeight;
-
-        //listCustom.OnChangeItem += OnChange;
-
-        listCustom.OnAdd = OnAdded;
-
-        listCustom.OnReorderItem += (evt, element, index) => { SaveTargetChanges(); };
-
-        listCustom.OnRemoveItem += (element, index) => { SaveTargetChanges(); };
-
-        root.RegisterCallback<ChangeEvent<string>>((evt) => { SaveTargetChanges(); });
-
-        listCustom.reOrderMode = CustomListView<Interaction>.ReOrderModes.animatedDynamic;
-
-        //cambiar colores multiplicando por alfa o sumando varios
-        //falta un recuadro
-        //falta seleccion multiple
-        //falta que elimine lo seleccionado
-        //DONE
-        //probar uxml compuestos
-        //falta reordenamiento en realtime y animado
-        //falta que haga highlight cuando pasas el mouse
-        //falta poder seleccionar
-
-
-        listCustom.Init(root.Q<VisualElement>("CustomListView"));
-
+        interaction.ShowGUI(root.Q("Interaction"), target);
+        
         LocalAndGlobalProperties properties = (LocalAndGlobalProperties)CreateInstance(typeof(LocalAndGlobalProperties));
 
         properties.CreateGUI(myTarget.local_properties, root.Q("LocalAndGlobalProperties"));
-
+        
         return root;
     }
 
-    private void SaveTargetChanges()
-    {
-        EditorUtility.SetDirty(target);
-    }
 
 
-    private Interaction OnAdded()
-    {
-        SaveTargetChanges();
-        return new Interaction();
-    }
 
-    private StyleLength itemHeight(int index)
-    {
-        return new StyleLength(StyleKeyword.Auto);
-    }
 
     private void OnAdded(IEnumerable<int> obj)
     {
         myTarget.interactions[myTarget.interactions.Count - 1] = new Interaction();
     }
 
-    private void UpdateSelector(Interaction interaction, int index, VisualElement element)
-    {
-        if (interaction != myTarget.interactions[index]) return;
-
-
-        if (!string.IsNullOrEmpty(interaction.type))
-        {
-            if (interaction.type == "Inventory" && !subTypeSelectors.ContainsKey(interaction))
-            {
-                subTypeSelectors.Add(interaction, (SelectInventory)CreateInstance(typeof(SelectInventory)));
-            }
-            else if (interaction.type != "Inventory" && subTypeSelectors.ContainsKey(interaction))
-            {
-                ((SelectInventory)subTypeSelectors[interaction]).Remove(element);
-                subTypeSelectors.Remove(interaction);
-            }
-        }
-
-        if (subTypeSelectors.ContainsKey(interaction))
-        {
-            ((SelectInventory)subTypeSelectors[interaction]).Add(interaction, element);
-        }
-    }
+    
 }
