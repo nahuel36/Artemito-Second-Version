4d47f68008e017ff2e98700b754c4ee9015b9da3
NahuelHawk <nmuchetti@purpletreestudio.com>
2024-02-10 02:01:07 -0300

    agregando settings

---
+++ Assets/Settings.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Settings", menuName = "Pnc/SettingsFile", order = 1)]
+public class Settings : ScriptableObject
+{
+    public enum SpeechStyle
+    {
+        LucasArts, 
+        Sierra,
+        Custom
+    }
+
+    public enum InteractionExecuteMethod
+    {
+        FirstActionThenObject,
+        FirstObjectThenAction
+    }
+    public enum PathFindingType
+    {
+        UnityNavigationMesh,
+        AronGranbergAStarPath,
+        Custom
+    }
+    public enum ObjetivePosition 
+    { 
+        fixedPosition,
+        overCursor
+    }
+    public Verb[] verbs;
+    public int verbIndex;
+    public GlobalPropertyConfig[] globalPropertiesConfig = new GlobalPropertyConfig[0];
+    public int global_propertiesIndex;
+    public PathFindingType pathFindingType;
+    public SpeechStyle speechStyle;
+    public InteractionExecuteMethod interactionExecuteMethod;
+    public ObjetivePosition objetivePosition;
+    public bool showNumbersInDialogOptions = false;
+    public bool alwaysShowAllVerbs = false;
+}
