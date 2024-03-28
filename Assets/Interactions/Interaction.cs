35bb0409c791d5afe7e5d6e7e28a32db9ae85d0c
hawk <nahuel36gg@gmail.com>
2024-03-28 20:26:42 -0300

    al duplicar una interaction faltaba que se dupliquen las properties

--- Assets/Interactions/Interaction.cs
+++ Assets/Interactions/Interaction.cs
@@ -32,6 +32,7 @@ public class Interaction
         interDestiny.subtype = interOrigin.subtype;
         interDestiny.subtypeObject = interOrigin.subtypeObject;
         interDestiny.action = interOrigin.action;
+        interDestiny.interaction_properties = interOrigin.interaction_properties;
         return interDestiny;
     }
 }
