3cf463d78a66e6f8c356266c41eb3cbc6960ccbe
NahuelHawk <nmuchetti@purpletreestudio.com>
2024-04-27 11:53:23 -0300

    character set local variable, primer paso, seleccion del character

--- Assets/Interactions/Character/SetLocalVariable/CharacterSetLocalVariable.cs
+++ Assets/Interactions/Character/SetLocalVariable/CharacterSetLocalVariable.cs
@@ -1,12 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
+#if UNITY_EDITOR
+using UnityEditor.UIElements;
+#endif
 
 [System.Serializable]
 public class CharacterSetLocalVariable : InteractionAction
 {
+    private LocalProperty propertyToSet;
+    public Character characterToSet;
+
     public override void ExecuteAction(List<InteractionProperty> properties, Interaction interaction)
     {
-        FindFirstObjectByType<TestSayScript>().GetComponent<Character>().local_properties[0].variablesContainer.SetValue("string", "pepe");
+        characterToSet.local_properties[0].variablesContainer.SetValue("string", "pepe");
     }
+
+    public override void SetEditorField(VisualElement visualElement, Interaction interaction)
+    {
+#if UNITY_EDITOR
+        base.SetEditorField(visualElement, interaction);
+
+        ObjectField characterField = new ObjectField();
+        characterField.objectType = typeof(Character);
+        characterField.value = characterToSet;
+        characterField.RegisterValueChangedCallback((newvalue) => { characterToSet = (Character)newvalue.newValue; }) ;
+        visualElement.Add(characterField);
+#endif
+    }
+
+
 }
