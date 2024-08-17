627cda23b4eedaef3d991b7e4d5d4185835a56a8
hawk <nahuel36gg@gmail.com>
2024-08-17 20:23:07 -0300

    arreglado problema al haber opciones distintas a los tipos de variables en el custom flag

--- Assets/Scripts/Utils/CustomEnumFlagsEditor.cs
+++ Assets/Scripts/Utils/CustomEnumFlagsEditor.cs
@@ -7,7 +7,6 @@ using UnityEditor;
 #endif
 using UnityEngine.UIElements;
 using System;
-using System.Net.WebSockets;
 
 [System.Serializable]
 public class CustomEnumFlagsEditor
@@ -31,7 +30,32 @@ public class CustomEnumFlagsEditor
 
     private void callback(ChangeEvent<Enum> evt, CustomEnumFlags value)
     {
-        value.SetIntValue(Convert.ToInt32((GenericEnum)evt.newValue));
+        if (value.type == CustomEnumFlags.contentType.variable)
+        {
+            List<int> indexs = new List<int>();
+            for (int i = 0; i < field.choices.Count; i++)
+            {
+                if ((Convert.ToInt32((GenericEnum)evt.newValue) & (1 << i)) != 0)
+                    indexs.Add(i);
+            }
+            int finalValue = 0;
+            VariableType[] variables = EnumerablesUtility.GetAllVariableTypes();
+            for (int i = 0; i < indexs.Count; i++)
+            {
+                for (int j = 0; j < variables.Length; j++)
+                {
+                    if (field.choices[indexs[i]] == variables[j].TypeName)
+                    {
+                        finalValue = finalValue | (1 << variables[j].Index);
+                    }
+                }
+            }
+            value.SetIntValue(finalValue);
+        }
+        else
+        { 
+            value.SetIntValue(Convert.ToInt32((GenericEnum)evt.newValue));
+        }
     }
 
     public void SetChoices(Func<int, string> func, int Lenght)
